dc7293fd05747330208d5009e8a7802ba01b17c1
Sammie <99089596+ConfusedSammie@users.noreply.github.com>
2024-06-10 19:21:57 -0400

    Issue #11

--- index.php
+++ index.php
@@ -15,26 +15,42 @@ include('functions/auth.php');
     <div class="row">
       <div class="col-md-6">
         <div class="card mb-4 shadow-sm homepageCard">
-          <div class='imageContainer'>
-            <img src="https://t4.ftcdn.net/jpg/05/51/07/67/360_F_551076710_Q675H2O06HaA9oDBijQ4A2h17emW3xIF.jpg" class="card-img-top" alt="Plants" style='width:80%;'>
-          </div>
-          <div class="card-body">
-            <h5 class="card-title">Plants Toolkit</h5>
-            <p class="card-text">Explore a variety of tools and resources to help you take care of your plants.test</p>
-            <a href="plants.php" class="btn btn-primary">Go to Plants Toolkit</a>
-          </div>
+          <a href="plants.php">
+            <div class='imageContainer'>
+              <img src="https://t4.ftcdn.net/jpg/05/51/07/67/360_F_551076710_Q675H2O06HaA9oDBijQ4A2h17emW3xIF.jpg" class="card-img-top" alt="Plants" >
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Plants Toolkit</h5>
+              <p class="card-text">Explore a variety of tools and resources to help you take care of your plants.</p>
+            </div>
+          </a>
         </div>
       </div>
       <div class="col-md-6">
         <div class="card mb-4 shadow-sm homepageCard">
-          <div class='imageContainer'>
-            <img src="images/catSleeping.png" class="card-img-top" alt="Cats" style='width:80%;'>
-          </div>
-          <div class="card-body">
-            <h5 class="card-title">Cats Toolkit</h5>
-            <p class="card-text">Find useful tools and resources for taking care of your cats.</p>
-            <a href="cats.php" class="btn btn-primary">Go to Cats Toolkit</a>
-          </div>
+          <a href="cats.php">
+            <div class='imageContainer'>
+              <img src="images/catSleeping.png" class="card-img-top" alt="Cats">
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Cats Toolkit</h5>
+              <p class="card-text">Find useful tools and resources for taking care of your cats.</p>
+            </div>
+          </a>
+        </div>
+      </div>
+
+      <div class="col-md-6">
+        <div class="card mb-4 shadow-sm homepageCard">
+          <a href="home.php">
+            <div class='imageContainer'>
+              <img src="images/blueprint.png" class="card-img-top" alt="Cats">
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Home Toolkit</h5>
+              <p class="card-text">Find useful tools and resources for taking care of your home.</p>
+            </div>
+          </a>
         </div>
       </div>
     </div>
